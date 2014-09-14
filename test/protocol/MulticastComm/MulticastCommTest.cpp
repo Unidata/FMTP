6e62c8b02f82dc8d62d60780124b599155400a46
Shawn <sschenshawn@gmail.com>
2014-09-14 04:10:24 -0400

    test for MulticastComm done

--- test/protocol/MulticastComm/MulticastCommTest.cpp
+++ test/protocol/MulticastComm/MulticastCommTest.cpp
@@ -5,30 +5,38 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+#define on 1
+#define off 0
+
 using namespace std;
 
 int main(int argc, char* argv[])
 {
     const string demoServHost = "rivanna.cs.virginia.edu";
     const int demoServPort = 1234;
-    char sendDataBuf[256], recvDataBuf[28];
-
-    // clear two buffers
-    bzero(sendDataBuf, 256);
-    bzero(recvDataBuf, 28);
+    const char sendDataBuf[] = "hello, multicast world!";
+    char recvDataBuf[256];
 
     struct sockaddr_in demo_sain;
     bzero(&demo_sain, sizeof(demo_sain));
-    char* if_name = "eth0";
+    const char if_name[] = "eth0";
     demo_sain.sin_family = AF_INET;
     demo_sain.sin_addr.s_addr = inet_addr("224.0.0.1");
     demo_sain.sin_port = htons(demoServPort);
 
     MulticastComm demoMcast;
-    int join_retval = demoMcast.JoinGroup((sockaddr *)demo_sain, sizeof(demo_sain), if_name);
+    // test JoinGroup()
+    int join_retval = demoMcast.JoinGroup((struct sockaddr *)&demo_sain, sizeof(demo_sain), if_name);
     if(join_retval == 0)
         cout << "UDP socket set, multicast group set." << endl;
 
-    //while(1);
+    // test SetLoopBack()
+    demoMcast.SetLoopBack(off);
+
+    while(1) {
+        // test SendData()
+        demoMcast.SendData(sendDataBuf, sizeof(sendDataBuf), 0, NULL);
+    }
+
     return 0;
 }
