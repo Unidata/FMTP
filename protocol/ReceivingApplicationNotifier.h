a2710664276aa7e9778440f82a160c8bf8522411
steve <steve@unidata.ucar.edu>
2014-10-08 14:01:18 -0600

    Changed API to separate files from memory-data

--- protocol/ReceivingApplicationNotifier.h
+++ protocol/ReceivingApplicationNotifier.h
@@ -17,44 +17,51 @@
 #include "vcmtp.h"
 #include "VcmtpFileEntry.h"
 
-#include <exception>
 #include <memory>
 
 /**
- * This base class notifies a receiving application about file events.
+ * This base class notifies a receiving application about events.
  */
 class ReceivingApplicationNotifier {
 public:
     virtual ~ReceivingApplicationNotifier() {};
 
     /**
-     * Notifies the receiving application about the beginning of a file.
+     * Notifies the receiving application about the beginning of a file
+     * transfer.
      *
      * @param[in,out] file_entry        The VCMTP entry for the file.
      * @retval        0                 Success.
      * @retval        -1                Failure.
      */
-    /*
-     * TODO: Have this method return a BofResponse that the VCMTPReceiver will
-     * subsequently use to dispose of data packets.
+    virtual void notify_of_bof(VcmtpMessageInfo& info) = 0;
+
+    /**
+     * Notifies the receiving application about the beginning of a memory data
+     * transfer.
+     *
+     * @param[in,out] file_entry        The VCMTP entry for the file.
+     * @retval        0                 Success.
+     * @retval        -1                Failure.
      */
-    virtual void notify_of_bof(VcmtpFileEntry& file_entry) = 0;
+    virtual void notify_of_bomd(VcmtpMessageInfo& info) = 0;
 
     /**
      * Notifies the receiving application about the complete reception of a
      * file.
      */
-    virtual void notify_of_eof(VcmtpFileEntry& file_entry) = 0;
+    virtual void notify_of_eof(VcmtpMessageInfo& info) = 0;
 
     /**
-     * Notifies the receiving application about a missed file.
+     * Notifies the receiving application about the complete reception of
+     * memory data.
      */
-    virtual void notify_of_missed_file(uint32_t fileId) = 0;
+    virtual void notify_of_eomd(VcmtpMessageInfo& info) = 0;
 
     /**
-     * Notifies the receiving application about an exception.
+     * Notifies the receiving application about a missed data-product.
      */
-    virtual void notify_of_exception(const std::exception& e) = 0;
+    virtual void notify_of_missed_product(uint32_t prodId) = 0;
 };
 
 #endif /* RECEIVING_APPLICATION_NOTIFIER_H_ */
