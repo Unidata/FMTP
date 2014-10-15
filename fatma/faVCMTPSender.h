4d425ea2987c5e1a8f50c5e14405f7e10b985a77
shawn <sschenshawn@gmail.com>
2014-10-14 21:07:33 -0400

    manually merge SendEOMD()

--- fatma/faVCMTPSender.h
+++ fatma/faVCMTPSender.h
@@ -30,22 +30,22 @@
 
 class faVCMTPSender {
 public:
-	faVCMTPSender(u_int64_t id,const char* recvAddr,const ushort recvPort);
+	faVCMTPSender(u_int64_t id);
 	virtual ~faVCMTPSender();
 	void SendBOFMessage(uint64_t dataLength, const char* fileName);
-	void SendBOMDMessage(uint64_t prodSize, char* prodName,int sizeOfProdName);
+	void SendBOMDMessage(uint64_t fileSize, char* prodId,int sizeOfProdId);
 	void CreateUPDSocket(const char* recvName,unsigned short int recvPort);
 	void sendFile(uint64_t dataLength, const char* fileName);
-	void sendMemoryData(void* data, uint64_t prodSize, string &prodName);
+	void sendMemoryData(void* data, uint64_t fileSize, string &prodId);
 	void DoMemoryTransfer(void* data, size_t length,u_int32_t start_seq_num);
-	void sendEOMDMessage();
+	
 private:
 	UdpComm* updSocket;
-	uint64_t prodId;
-	//	char  fileName[256];
-	//	uint64_t fileSize;
-	//	uint64_t seq_num;
-	//	char prodName[256];
+	uint64_t fileId;
+	char  fileName[256];
+	uint64_t fileSize;
+	uint64_t seq_num;
+	char prodId[256];
 };
 
 #endif /* faVCMTPSender_H_ */
--- fatma/test.cpp
+++ fatma/test.cpp
@@ -1,6 +1,6 @@
 /*
  *test.cpp
- 
+
  *
  *  Created on: Oct 4, 2014
  *      Author: fatmaal-ali
@@ -15,15 +15,18 @@
 int main()
 {
 	const u_int64_t id=1;
-	string prodName="FHA2";
-	
+	string prodId="ldm checksum";
+
 	faVCMTPSender* sender;
-	sender= new faVCMTPSender(id,"128.143.137.117",5173);
+	sender= new faVCMTPSender(id);
 	cout<<"main(): create new vcmtp sender with file id = "<<id<<endl;
 	//pass the address and port number of the receiver
 	//sender->CreateUPDSocket("128.143.137.117",5173);
-	char *filename = "/home/fatma/FatmaVCMTP/FHA2";
+	sender->CreateUPDSocket("127.0.0.1",5173);
+	//char *filename = "/home/shawn/vcmtp/fatma/FHA2";
+	char *filename = "memdata";
 	int fd;
+	//	if(int fd = open("FHA2", O_RDONLY)<0)
 	fd = open(filename,O_RDONLY);
 	if(fd>0)
 	{
@@ -31,18 +34,18 @@ int main()
 		data = (char*) mmap(0, 2856, PROT_READ, MAP_FILE | MAP_SHARED, fd,0);
 		if (data == MAP_FAILED)
 			cout<<"file map failed"<<endl;
-		
+
 		//	while(1){
-		sender->sendMemoryData( data,2856, prodName);
+		sender->sendMemoryData( data,2856, prodId);
 		//	sleep(1);
 		//	}
-		
+
 		munmap(data, 2856);
 		close(fd);
 	}
 	else
 		cout<<"test::main()::open(): error"<<endl;
-	
+
 	return 0;
 }
 
