c0e0349f3cd98734791b00aaedf96a9f8bac503b
FatmaUVA <eng.f_06@hotmail.com>
2014-10-19 00:01:05 -0400

    added TCP server address and port to the constructor

--- VCMTPv3/fatma/test.cpp
+++ VCMTPv3/fatma/test.cpp
@@ -1,5 +1,5 @@
 /*
-*test.cpp
+ *test.cpp
  *
  *  Created on: Oct 17, 2014
  *      Author: fatmaal-ali
@@ -17,28 +17,28 @@ int main()
 	unsigned int metaSize=sizeof(data);
 	
 	vcmtpSendv3* sender;
-	sender= new vcmtpSendv3("128.143.137.117",5173,0);
+	sender= new vcmtpSendv3("192.168.56.101",1234,"128.143.137.117",5173,0);
 	char *filename = "/home/fatma/vcmtpv3/FHA2";
 	int fd;
 	fd = open(filename,O_RDONLY);
 	if(fd>0)
 	{
-	void* data;
-	data = (char*) mmap(0, 2856, PROT_READ, MAP_FILE | MAP_SHARED, fd,0);
-	if (data == MAP_FAILED)
-		cout<<"file map failed"<<endl;
-
-//	while(1){
-	sender->sendProduct(data,2856, metadata,metaSize);
-//	sleep(1);
-//	}
-
-	munmap(data, 2856);
-	close(fd);
+		void* data;
+		data = (char*) mmap(0, 2856, PROT_READ, MAP_FILE | MAP_SHARED, fd,0);
+		if (data == MAP_FAILED)
+			cout<<"file map failed"<<endl;
+		
+		//	while(1){
+		sender->sendProduct(data,2856, metadata,metaSize);
+		//	sleep(1);
+		//	}
+		
+		munmap(data, 2856);
+		close(fd);
 	}
 	else
 		cout<<"test::main()::open(): error"<<endl;
-
+	
 	return 0;
 }
 
