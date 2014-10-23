8d3993a71ad5c79970b3b1fcd5b5edafb610b5d4
shawn <sschenshawn@gmail.com>
2014-10-23 16:18:52 -0400

    roll back to udp unicast

--- VCMTPv3/shawn/recv/main.cpp
+++ VCMTPv3/shawn/recv/main.cpp
@@ -6,8 +6,9 @@ int main(int argc, char* argv[])
 {
     string tcpAddr                 = "127.0.0.1";
     const unsigned short tcpPort   = 5000;
-    string mcastAddr               = "233.0.225.123";
-    const unsigned short mcastPort = 5173;
+    //string mcastAddr               = "233.0.225.123";
+    string mcastAddr               = "0.0.0.0";
+    const unsigned short mcastPort = 388;
 
     vcmtpRecvv3 vcmtpRecvv3(tcpAddr, tcpPort, mcastAddr, mcastPort);
     vcmtpRecvv3.Start();
