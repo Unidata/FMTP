8150c471fcc6487904e42f03978e14ff2d321757
Shawn <sschenshawn@gmail.com>
2014-11-02 22:47:22 -0500

    build documentation for vcmtpBase

--- VCMTPv3/vcmtpBase.h
+++ VCMTPv3/vcmtpBase.h
@@ -1,13 +1,28 @@
-/*
+/**
  * Copyright (C) 2014 University of Virginia. All rights reserved.
- * @licence: Published under GPLv3
  *
- * @filename: vcmtpBase.h
+ * @file      vcmtpBase.h
+ * @author    Shawn Chen <sc7cq@virginia.edu>
+ * @version   1.0
+ * @date      Oct 7, 2014
  *
- * @history:
- *      Created on : Oct 7, 2014
- *      Author     : Shawn <sc7cq@virginia.edu>
+ * @section   LICENSE
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or（at your option）
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details at http://www.gnu.org/copyleft/gpl.html
+ *
+ * @brief     Define the interfaces of VCMTPv3 basics.
+ *
+ * Definition of control message definition, header struct and message length.
  */
+
 
 #ifndef VCMTPBASE_H_
 #define VCMTPBASE_H_
@@ -18,8 +33,11 @@
 #include <string.h>
 
 
+/**
+ * struct of Vcmtp header
+ */
 typedef struct VcmtpPacketHeader {
-    uint32_t   prodindex;    // identify both file and memdata by this indexid.
+    uint32_t   prodindex;    /*!< identify both file and memdata by prodindex. */
     uint32_t   seqnum;
     uint16_t   payloadlen;
     uint16_t   flags;
@@ -28,10 +46,13 @@ typedef struct VcmtpPacketHeader {
 const int MAX_VCMTP_PACKET_LEN = 1460;
 const int VCMTP_HEADER_LEN     = sizeof(VcmtpHeader);
 const int VCMTP_DATA_LEN       = MAX_VCMTP_PACKET_LEN - VCMTP_HEADER_LEN;
-const int AVAIL_BOP_LEN        = VCMTP_DATA_LEN - 4 - 2; // prosize is 4 bytes, metasize is 2 bytes
+const int AVAIL_BOP_LEN        = VCMTP_DATA_LEN - 4 - 2; /*!< prosize is 4 bytes, metasize is 2 bytes */
 
+/**
+ * structure of Begin-Of-Product message
+ */
 typedef struct VcmtpBOPMessage {
-    uint32_t   prodsize; //max size is 4GB
+    uint32_t   prodsize;     /*!< support 4GB maximum */
     uint16_t   metasize;
     char       metadata[AVAIL_BOP_LEN];
 } BOPMsg;
