a50b1271414ad8cd578f3b4b1f4ee76bb5392ae5
Shawn <sschenshawn@gmail.com>
2014-11-17 22:50:52 -0500

    let TCP listen on anyaddr

--- VCMTPv3/sender/test.cpp
+++ VCMTPv3/sender/test.cpp
@@ -17,7 +17,7 @@ int main()
 	unsigned int metaSize=sizeof(data);
 
 	vcmtpSendv3* sender;
-	sender = new vcmtpSendv3("127.0.0.1", 1234, "128.143.137.117", 5173, 0);
+	sender = new vcmtpSendv3("0.0.0.0", 1234, "233.0.225.123", 5173, 0);
 	char *filename = "TESTDATA";
 	int fd;
 	fd = open(filename,O_RDONLY);
