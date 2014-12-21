344a7472f3a97dfb103f7ee4066d533cb11aa7b0
Shawn <sschenshawn@gmail.com>
2014-12-21 00:26:26 -0500

    working on docs of senderMetadata class

--- VCMTPv3/sender/senderMetadata.cpp
+++ VCMTPv3/sender/senderMetadata.cpp
@@ -1,43 +1,90 @@
+/**
+ * Copyright (C) 2014 University of Virginia. All rights reserved.
+ *
+ * @file      senderMetadata.cpp
+ * @author    Shawn Chen <sc7cq@virginia.edu>
+ * @version   1.0
+ * @date      Nov 28, 2014
+ *
+ * @section   LICENSE
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or（at your option）
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details at http://www.gnu.org/copyleft/gpl.html
+ *
+ * @brief     Implement the interfaces and structures of VCMTPv3 sender side
+ *            retransmission metadata.
+ *
+ * VCMTPv3 sender side retransmission metadata method functions. It supports
+ * add/rm, query and modify operations.
+ */
+
+
+
 #include "senderMetadata.h"
 #include <iostream>
 
 #define NULL 0
 
+
+/**
+ * Construct the senderMetadata class, initialize the index-metadata mapping
+ * lock.
+ *
+ * @param[in] none
+ */
 senderMetadata::senderMetadata()
 {
-	pthread_rwlock_init(&indexMetaMapLock, NULL);
+    pthread_rwlock_init(&indexMetaMapLock, NULL);
 }
 
 
+/**
+ * Destruct the senderMetadata class, destroy the index-metadata mapping lock.
+ * Clear the whole prodindex-metadata map.
+ *
+ * @param[in] none
+ */
 senderMetadata::~senderMetadata()
 {
-	for (map<uint32_t, RetxMetadata*>::iterator it = indexMetaMap.begin();
-		 it != indexMetaMap.end(); ++it)
-	{
-		delete(it->second);
-		//TODO: need to erase(it) ?
-	}
-	pthread_rwlock_destroy(&indexMetaMapLock);
+    for (map<uint32_t, RetxMetadata*>::iterator it = indexMetaMap.begin();
+         it != indexMetaMap.end(); ++it)
+    {
+        delete(it->second);
+        indexMetaMap.erase(it);
+    }
+    pthread_rwlock_destroy(&indexMetaMapLock);
 }
 
 
+/**
+ *
+ *
+ * @param[in] prodindex
+ */
 RetxMetadata* senderMetadata::getMetadata(uint32_t prodindex)
 {
-	RetxMetadata* temp = NULL;
-	map<uint32_t, RetxMetadata*>::iterator it;
-	pthread_rwlock_rdlock(&indexMetaMapLock);
-	if ((it = indexMetaMap.find(prodindex)) != indexMetaMap.end())
-		temp = it->second;
-	pthread_rwlock_unlock(&indexMetaMapLock);
-	return temp;
+    RetxMetadata* temp = NULL;
+    map<uint32_t, RetxMetadata*>::iterator it;
+    pthread_rwlock_rdlock(&indexMetaMapLock);
+    if ((it = indexMetaMap.find(prodindex)) != indexMetaMap.end())
+        temp = it->second;
+    pthread_rwlock_unlock(&indexMetaMapLock);
+    return temp;
 }
 
 
 void senderMetadata::addRetxMetadata(RetxMetadata* ptrMeta)
 {
-	pthread_rwlock_wrlock(&indexMetaMapLock);
-	indexMetaMap[ptrMeta->prodindex] = ptrMeta;
-	pthread_rwlock_unlock(&indexMetaMapLock);
+    pthread_rwlock_wrlock(&indexMetaMapLock);
+    indexMetaMap[ptrMeta->prodindex] = ptrMeta;
+    pthread_rwlock_unlock(&indexMetaMapLock);
 }
 
 
--- VCMTPv3/sender/senderMetadata.h
+++ VCMTPv3/sender/senderMetadata.h
@@ -1,3 +1,32 @@
+/**
+ * Copyright (C) 2014 University of Virginia. All rights reserved.
+ *
+ * @file      senderMetadata.h
+ * @author    Shawn Chen <sc7cq@virginia.edu>
+ * @version   1.0
+ * @date      Nov 28, 2014
+ *
+ * @section   LICENSE
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or（at your option）
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details at http://www.gnu.org/copyleft/gpl.html
+ *
+ * @brief     Define the interfaces and structures of VCMTPv3 sender side
+ *            retransmission metadata.
+ *
+ * VCMTPv3 sender side retransmission data structures and interfaces, including
+ * the timeout period value, unfinished set and product pointers.
+ */
+
+
+
 #ifndef SENDERMETADATA_H_
 #define SENDERMETADATA_H_
 
@@ -9,20 +38,20 @@
 using namespace std;
 
 struct RetxMetadata {
-	uint32_t  prodindex;
+    uint32_t  prodindex;
     /** recording the whole product size (for timeout factor use) */
     uint32_t  prodLength;
-    clock_t   mcastStartTime;	 /*!< multicasting start time */
-    clock_t   mcastEndTime; 	 /*!< multicasting end time */
-    float	  retxTimeoutRatio;	 /*!< ratio to scale timeout time */
-    float     retxTimeoutPeriod; 	 /*!< timeout time in seconds */
-    void* 	  dataprod_p; 		 /*!< pointer to the data product */
-    set<int>  unfinReceivers;	 /*!< unfinished receiver set indexed by socket id */
+    clock_t   mcastStartTime;    /*!< multicasting start time */
+    clock_t   mcastEndTime;      /*!< multicasting end time */
+    float     retxTimeoutRatio;  /*!< ratio to scale timeout time */
+    float     retxTimeoutPeriod; /*!< timeout time in seconds */
+    void*     dataprod_p;        /*!< pointer to the data product */
+    set<int>  unfinReceivers;    /*!< unfinished receiver set indexed by socket id */
 
 
     RetxMetadata(): prodindex(0), prodLength(0), mcastStartTime(0.0),
-    				  mcastEndTime(0.0), retxTimeoutRatio(20.0),
-					  retxTimeoutPeriod(99999999999.0), dataprod_p(NULL) {}
+                    mcastEndTime(0.0), retxTimeoutRatio(20.0),
+                    retxTimeoutPeriod(99999999999.0), dataprod_p(NULL) {}
     virtual ~RetxMetadata() {}
 };
 
@@ -40,7 +69,7 @@ public:
 private:
     /** first: prodindex; second: pointer to metadata of the specified prodindex */
     map<uint32_t, RetxMetadata*> indexMetaMap;
-    pthread_rwlock_t 		     indexMetaMapLock;
+    pthread_rwlock_t             indexMetaMapLock;
     bool rmRetxMetadataNoLock(uint32_t prodindex);
 };
 
