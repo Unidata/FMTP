e5d48631339135780fd05c6fd2cd2b8329581885
steve <steve@unidata.ucar.edu>
2015-01-22 12:18:23 -0700

    Added mutex to TcpRecv and refactored.

--- VCMTPv3/receiver/TcpRecv.cpp
+++ VCMTPv3/receiver/TcpRecv.cpp
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include <iostream>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -11,66 +12,138 @@
 using namespace std;
 
 TcpRecv::TcpRecv(string tcpAddr, unsigned short tcpPort)
+:
+    mutex()
 {
     (void) memset((char *) &servAddr, 0, sizeof(servAddr));
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(sockfd < 0)
-        std::cout << "TcpRecv::TcpRecv() error creating socket" << std::endl;
     servAddr.sin_family = AF_INET;
     servAddr.sin_addr.s_addr = inet_addr(tcpAddr.c_str());
     servAddr.sin_port = htons(tcpPort);
-    if(connect(sockfd, (struct sockaddr *) &servAddr, sizeof(servAddr)) < 0)
-        std::cout << "TcpRecv:TcpRecv() error connecting to sender" << std::endl;
+    initSocket();
 }
-
 
 TcpRecv::~TcpRecv()
 {
-    close(sockfd);
+    close(getSocket());
 }
 
-
+/**
+ * Sends a header and a payload on the TCP connection. Blocks until the packet
+ * is sent or a severe error occurs. Re-establishes the TCP connection if
+ * necessary.
+ *
+ * @param[in] header   Header.
+ * @param[in] headLen  Length of the header in bytes.
+ * @param[in] payload  Payload.
+ * @param[in] payLen   Length of the payload in bytes.
+ * @retval    -1       O/S failure.
+ * @return             Number of bytes sent.
+ */
 ssize_t TcpRecv::sendData(void* header, size_t headLen, char* payload,
                           size_t payLen)
 {
+    ssize_t      nbytes;
     struct iovec iov[2];
+
     iov[0].iov_base = header;
     iov[0].iov_len  = headLen;
     iov[1].iov_base = payload;
     iov[1].iov_len  = payLen;
 
-    int retval = writev(sockfd, iov, 2);
-    // TODO: what if reconnect() succeeded but still can't write?
-    while (retval < 0) {
+    for (;;) {
+        nbytes = writev(getSocket(), iov, 2);
+        if (nbytes != -1)
+            break;
         reconnect();
-        retval = writev(sockfd, iov, 2);
     }
-    return retval;
+
+    return nbytes; // Eclipse wants to see a return
 }
 
-
+/**
+ * Receives a header and a payload on the TCP connection. Blocks until the
+ * packet is received or a severe error occurs. Re-establishes the TCP
+ * connection if necessary.
+ *
+ * @param[in] header   Header.
+ * @param[in] headLen  Length of the header in bytes.
+ * @param[in] payload  Payload.
+ * @param[in] payLen   Length of the payload in bytes.
+ * @retval    -1       O/S failure.
+ * @return             Number of bytes received.
+ */
 ssize_t TcpRecv::recvData(void* header, size_t headLen, char* payload,
                           size_t payLen)
 {
+    ssize_t      nbytes;
     struct iovec iov[2];
+
     iov[0].iov_base = header;
     iov[0].iov_len  = headLen;
     iov[1].iov_base = payload;
     iov[1].iov_len  = payLen;
 
-    int retval = readv(sockfd, iov, 2);
-    while (retval < 0) {
-        retval = readv(sockfd, iov, 2);
+    for (;;) {
+        nbytes = readv(getSocket(), iov, 2);
+        if (nbytes != -1)
+            break;
+        reconnect();
     }
-    return retval;
+
+    return nbytes; // Eclipse wants to see a return
 }
 
-
-bool TcpRecv::reconnect()
+/**
+ * Initializes the TCP connection. Blocks until the connection is established
+ * or a severe error occurs.
+ *
+ * @throws std::system_error  if a system error occurs.
+ */
+void TcpRecv::initSocket()
 {
-    close(sockfd);
-    /** keep trying to re-connect until success */
-    while( connect(sockfd, (struct sockaddr *) &servAddr,
-            sizeof(servAddr)) < 0 );
-    return true;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0)
+        throw std::system_error(errno, std::system_category(),
+                "TcpRecv::TcpRecv() error creating socket");
+
+    while (connect(sockfd, (struct sockaddr*)&servAddr, sizeof(servAddr))) {
+        if (errno != ECONNREFUSED && errno != ETIMEDOUT &&
+                errno != ECONNRESET && errno != EHOSTUNREACH) {
+            throw std::system_error(errno, std::system_category(),
+                    "TcpRecv:TcpRecv() error connecting to sender");
+        }
+        sleep(30);
+    }
+}
+
+/**
+ * Ensures that the TCP connection is established. Blocks until the connection
+ * is established or a severe error occurs. Does nothing if the connection is
+ * OK. This method is thread-safe.
+ *
+ * @throws std::system_error  if a system error occurs.
+ */
+void TcpRecv::reconnect()
+{
+    int                          status;
+    socklen_t                    len = sizeof(status);
+    std::unique_lock<std::mutex> lock(mutex);
+
+    if (getsockopt(sockfd, SOL_SOCKET, SO_ERROR, &status, &len) || status) {
+        close(sockfd);
+        initSocket();
+    }
+}
+
+/**
+ * Returns the socket corresponding to the TCP connection. This method is
+ * thread-safe.
+ *
+ * @return  The corresponding socket.
+ */
+int TcpRecv::getSocket()
+{
+    std::unique_lock<std::mutex> lock(mutex);
+    return sockfd;
 }
--- VCMTPv3/receiver/TcpRecv.h
+++ VCMTPv3/receiver/TcpRecv.h
@@ -1,6 +1,7 @@
 #ifndef TCPRECV_H_
 #define TCPRECV_H_
 
+#include <mutex>
 #include <string>
 
 class TcpRecv
@@ -8,15 +9,61 @@ class TcpRecv
     public:
         TcpRecv(std::string tcpAddr, unsigned short tcpPort);
         ~TcpRecv();
+        /**
+         * Sends a header and a payload on the TCP connection. Blocks until the packet
+         * is sent or a severe error occurs. Re-establishes the TCP connection if
+         * necessary.
+         *
+         * @param[in] header   Header.
+         * @param[in] headLen  Length of the header in bytes.
+         * @param[in] payload  Payload.
+         * @param[in] payLen   Length of the payload in bytes.
+         * @retval    -1       O/S failure.
+         * @return             Number of bytes sent.
+         */
         ssize_t sendData(void* header, size_t headLen, char* payload,
                          size_t payLen);
+        /**
+         * Receives a header and a payload on the TCP connection. Blocks until the
+         * packet is received or a severe error occurs. Re-establishes the TCP
+         * connection if necessary.
+         *
+         * @param[in] header   Header.
+         * @param[in] headLen  Length of the header in bytes.
+         * @param[in] payload  Payload.
+         * @param[in] payLen   Length of the payload in bytes.
+         * @retval    -1       O/S failure.
+         * @return             Number of bytes received.
+         */
         ssize_t recvData(void* header, size_t headLen, char* payload,
                          size_t payLen);
     private:
-        int sockfd;
-        struct sockaddr_in servAddr;
+        int                     sockfd;
+        struct sockaddr_in      servAddr;
+        std::mutex              mutex;
 
-        bool reconnect();
+        /**
+         * Initializes the TCP connection. Blocks until the connection is established
+         * or a severe error occurs.
+         *
+         * @throws std::system_error  if a system error occurs.
+         */
+        void initSocket();
+        /**
+         * Ensures that the TCP connection is established. Blocks until the connection
+         * is established or a severe error occurs. Does nothing if the connection is
+         * OK. This method is thread-safe.
+         *
+         * @throws std::system_error  if a system error occurs.
+         */
+        void reconnect();
+        /**
+         * Returns the socket corresponding to the TCP connection. This method is
+         * thread-safe.
+         *
+         * @return  The corresponding socket.
+         */
+        int  getSocket();
 };
 
 #endif /* TCPRECV_H_ */
