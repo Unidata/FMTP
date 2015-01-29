9ad50108e0a6e21f6fd92c7839b7a7e5c55af777
Shawn <sschenshawn@gmail.com>
2015-01-28 21:23:49 -0500

    parsing the 4 fields in vcmtp header

--- wireshark-dissector/packet-vcmtp.c
+++ wireshark-dissector/packet-vcmtp.c
@@ -6,6 +6,9 @@
 
 static int proto_vcmtp = -1;
 static int hf_vcmtp_prodindex = -1;
+static int hf_vcmtp_seqnum = -1;
+static int hf_vcmtp_paylen = -1;
+static int hf_vcmtp_flags = -1;
 static gint ett_vcmtp = -1;
 
 static void dissect_vcmtp(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
@@ -20,6 +23,9 @@ static void dissect_vcmtp(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
         ti = proto_tree_add_item(tree, proto_vcmtp, tvb, 0, -1, ENC_NA);
         vcmtp_tree = proto_item_add_subtree(ti, ett_vcmtp);
         proto_tree_add_item(vcmtp_tree, hf_vcmtp_prodindex, tvb, 0, 4, ENC_BIG_ENDIAN);
+        proto_tree_add_item(vcmtp_tree, hf_vcmtp_seqnum, tvb, 0, 4, ENC_BIG_ENDIAN);
+        proto_tree_add_item(vcmtp_tree, hf_vcmtp_paylen, tvb, 0, 2, ENC_BIG_ENDIAN);
+        proto_tree_add_item(vcmtp_tree, hf_vcmtp_flags, tvb, 0, 2, ENC_BIG_ENDIAN);
     }
 }
 
@@ -29,6 +35,24 @@ void proto_register_vcmtp(void)
         { &hf_vcmtp_prodindex,
             { "VCMTP ProdIndex", "vcmtp.prodindex",
             FT_UINT32, BASE_DEC,
+            NULL, 0x0,
+            NULL, HFILL }
+        },
+        { &hf_vcmtp_seqnum,
+            { "VCMTP Sequence Number", "vcmtp.seqnum",
+            FT_UINT32, BASE_DEC,
+            NULL, 0x0,
+            NULL, HFILL }
+        },
+        { &hf_vcmtp_paylen,
+            { "VCMTP Payload Length", "vcmtp.paylen",
+            FT_UINT16, BASE_DEC,
+            NULL, 0x0,
+            NULL, HFILL }
+        },
+        { &hf_vcmtp_flags,
+            { "VCMTP Flags", "vcmtp.flags",
+            FT_UINT16, BASE_DEC,
             NULL, 0x0,
             NULL, HFILL }
         }
