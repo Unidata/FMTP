0e45d1eb710808b7322c831f5c16397b0caebacf
Shawn <sschenshawn@gmail.com>
2015-02-10 14:57:03 -0500

    abstract testapp

--- VCMTPv3/sender/test.cpp
+++ VCMTPv3/sender/test.cpp
@@ -19,22 +19,23 @@ int main()
 	vcmtpSendv3* sender;
 	//sender = new vcmtpSendv3("192.168.0.101", 1234, "128.143.137.117", 5173, 0);
 	sender = new vcmtpSendv3("127.0.0.1", 1234, "233.0.225.123", 5173, 0, 0);
-	char *filename = "RANDDATA";
+	char *filename = "TEST1B";
+    size_t datasize = 1
 	int fd;
 	fd = open(filename,O_RDONLY);
 	if(fd>0)
 	{
 		void* data;
-		data = (char*) mmap(0, 5792, PROT_READ, MAP_FILE | MAP_SHARED, fd,0);
+		data = (char*) mmap(0, datasize, PROT_READ, MAP_FILE | MAP_SHARED, fd,0);
 		if (data == MAP_FAILED)
 			std::cout << "file map failed" << std::endl;
 
         sender->startCoordinator();
         sleep(2);
-		sender->sendProduct(data, 5792, metadata, metaSize);
+		sender->sendProduct(data, datasize, metadata, metaSize);
         while(1);
 
-		munmap(data, 5792);
+		munmap(data, datasize);
 		close(fd);
 	}
 	else
