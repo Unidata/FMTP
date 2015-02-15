d01563257269d97129b1b6e16901a5dd3aa63e18
shawn <sschenshawn@gmail.com>
2015-02-14 20:01:17 -0500

    re-write testRecvApp to take command line arguments

--- VCMTPv3/receiver/testRecvApp.cpp
+++ VCMTPv3/receiver/testRecvApp.cpp
@@ -1,18 +1,53 @@
+/**
+ * Copyright (C) 2014 University of Virginia. All rights reserved.
+ *
+ * @file      testRecvApp.cpp
+ * @author    Shawn Chen <sc7cq@virginia.edu>
+ * @version   1.0
+ * @date      Feb 14, 2015
+ *
+ * @section   LICENSE
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or（at your option）
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details at http://www.gnu.org/copyleft/gpl.html
+ *
+ * @brief     A testing application to use the receiver side protocol.
+ *
+ * Since the LDM could be to heavy to use for testing purposes only. This
+ * testRecvApp is written as a replacement. It can create an instance of the
+ * vcmtpRecvv3 instance and mock the necessary components to get it functioning.
+ */
+
+
 #include <iostream>
 #include <string>
 #include "vcmtpRecvv3.h"
-#include <unistd.h>
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
+    if (argc < 4) {
+        std::cerr << "ERROR: Insufficient arguments." << std::endl;
+        return 1;
+    }
+    /*
     string tcpAddr                 = "127.0.0.1";
     const unsigned short tcpPort   = 1234;
-    //string mcastAddr               = "128.143.137.117"; /** IP addr of rivanna */
     string mcastAddr               = "233.0.225.123";
-    //string mcastAddr               = "172.25.98.111";
     const unsigned short mcastPort = 5173;
+    */
+    string tcpAddr(argv[1]);
+    const unsigned short tcpPort = (unsigned short)atoi(argv[2]);
+    string mcastAddr(argv[3]);
+    const unsigned short mcastPort = (unsigned short)atoi(argv[4]);
 
     vcmtpRecvv3 vcmtpRecvv3(tcpAddr, tcpPort, mcastAddr, mcastPort);
     vcmtpRecvv3.Start();
