90559a04f3b637dade907f1febfc988014538727
shawn <sschenshawn@gmail.com>
2015-03-11 23:14:53 -0400

    add example code of keep alive

--- test/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "VCMTPReceiver.h"
-
-int main(int argc, char* argv[])
-{
-    string servAddr = "rivanna.cs.virginia.edu";
-    VCMTPReceiver receiver(servAddr, 5000, );
-    return 0;
-}
