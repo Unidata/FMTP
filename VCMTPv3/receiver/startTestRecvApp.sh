5afbd592df84816c92aeb9e1077b16e7115d76e9
Shawn <sschenshawn@gmail.com>
2015-05-18 19:59:15 -0400

    adapting shared disk testbed, get IP address of each node

--- VCMTPv3/receiver/startTestRecvApp.sh
+++ VCMTPv3/receiver/startTestRecvApp.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+# get the second IP address in OS
+#bindip=`hostname -I | awk -F ' ' '{print $2}'`
+#./testRecvApp 127.0.0.1 1234 233.0.225.123 5173 $bindip
 ./testRecvApp 127.0.0.1 1234 233.0.225.123 5173 0.0.0.0
