158d5f7316bb5b112e0ee9086cb3600f2491c39a
Shawn <sschenshawn@gmail.com>
2015-05-26 01:48:14 -0400

    added license info

--- test/misc/udp_send.cpp
+++ test/misc/udp_send.cpp
@@ -1,3 +1,27 @@
+/**
+ * Copyright (C) 2015 University of Virginia. All rights reserved.
+ *
+ * @file      udp_send.cpp
+ * @author    Shawn Chen <sc7cq@virginia.edu>
+ * @version   1.0
+ * @date      Mar 25, 2015
+ *
+ * @section   LICENSE
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or（at your option）
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details at http://www.gnu.org/copyleft/gpl.html
+ *
+ * @brief     UDP sending test program
+ */
+
+
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <stdio.h>
