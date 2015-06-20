6c0b51e3f860a90d3061d2f84f99b7335fd27429
Shawn <sschenshawn@gmail.com>
2015-06-19 21:25:23 -0400

    put rate shaper in VCMTPv3

---
+++ VCMTPv3/RateShaper/RateShaper.h
@@ -0,0 +1,52 @@
+/**
+ * Copyright (C) 2015 University of Virginia. All rights reserved.
+ *
+ * @file      RateShaper.h
+ * @author    Shawn Chen <sc7cq@virginia.edu>
+ * @version   1.0
+ * @date      June 19, 2015
+ *
+ * @section   LICENSE
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or（at your option）
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details at http://www.gnu.org/copyleft/gpl.html
+ *
+ * @brief     Rate shaper header file.
+ */
+
+#ifndef VCMTP_VCMTPV3_RATESHAPER_H_
+#define VCMTP_VCMTPV3_RATESHAPER_H_
+
+
+#include <time.h>
+#include <chrono>
+
+typedef std::chrono::high_resolution_clock HRC;
+
+
+class RateShaper {
+public:
+    RateShaper();
+    ~RateShaper();
+    /* sets the expected rate in bits/sec */
+    void SetRate(double rate_bps);
+    void CalPeriod(unsigned int size);
+    void Sleep();
+
+private:
+    double period;
+    double sleeptime;
+    double rate;
+    unsigned int txsize;
+    HRC::time_point start_time;
+    HRC::time_point end_time;
+};
+
+#endif /* VCMTP_VCMTPV3_RATESHAPER_H_ */
