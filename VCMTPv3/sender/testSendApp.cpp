b8fd4aa658115e7baa234801477c3629896e3ca1
Shawn <sschenshawn@gmail.com>
2015-06-21 00:01:44 -0400

    updated license

--- VCMTPv3/sender/testSendApp.cpp
+++ VCMTPv3/sender/testSendApp.cpp
@@ -8,15 +8,18 @@
  *
  * @section   LICENSE
  *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation; either version 2 of the License, or（at your option）
- * any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details at http://www.gnu.org/copyleft/gpl.html
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @brief     A testing application to use the sender side protocol.
  *
@@ -116,7 +119,7 @@ int main(int argc, char const* argv[])
         new vcmtpSendv3(tcpAddr.c_str(), tcpPort, mcastAddr.c_str(), mcastPort,
                         0, 1, ifAddr.c_str());
 
-    sender->SetSendRate(20000000);
+    sender->SetSendRate(2000000);
     sender->Start();
     sleep(2);
 
