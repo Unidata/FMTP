81d29681c9a902772509c268e6e020642ef9e5bf
Shawn <sschenshawn@gmail.com>
2015-07-28 02:18:53 -0400

    updated auto script to run experiments

--- VCMTPv3/sender/startTestSendApp.sh
+++ VCMTPv3/sender/startTestSendApp.sh
@@ -8,12 +8,11 @@ ext="${fullname##*.}"
 run="_run"
 dot="."
 
-mkdir sender-log
 for i in {1..10}
 do
     ./testSendApp 127.0.0.1 1234 233.0.225.123 5173 0.0.0.0 day1NGRID.csv
-    mv VCMTPv3_SENDER.log sender-log/$filename$run$i$dot$ext
+    mv VCMTPv3_SENDER.log $filename$run$i$dot$ext
     sleep 2
 done
 
-mail -s "Experiment done" sschenshawn@gmail.com < /dev/null
+#mail -s "Experiment done" sschenshawn@gmail.com < /dev/null
