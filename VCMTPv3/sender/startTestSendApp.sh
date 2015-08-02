b099b8b2668cca7b53090f2df0a315d902060dda
Shawn <sschenshawn@gmail.com>
2015-08-02 02:08:07 -0400

    improved startTestSendApp.sh

--- VCMTPv3/sender/startTestSendApp.sh
+++ VCMTPv3/sender/startTestSendApp.sh
@@ -8,9 +8,15 @@ ext="${fullname##*.}"
 run="_run"
 dot="."
 
+TCPADDR="10.10.1.1"
+TCPPORT="1234"
+MCASTADR="224.0.0.1"
+MCASTPORT="5173"
+INTERFACE="10.10.1.1"
+
 for i in {1..10}
 do
-    ./testSendApp 127.0.0.1 1234 233.0.225.123 5173 0.0.0.0 day1NGRID.csv
+    ./testSendApp $TCPADDR $TCPPORT $MCASTADDR $MCASTPORT $INTERFACE day1NGRID.csv
     mv VCMTPv3_SENDER.log $filename$run$i$dot$ext
     sleep 2
 done
