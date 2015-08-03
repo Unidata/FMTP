0db984a04e9339f9b1c94e363da0d3a0a7d2b4d6
Shawn <sschenshawn@gmail.com>
2015-08-03 03:52:06 -0400

    no need to rename log files

--- VCMTPv3/sender/startTestSendApp.sh
+++ VCMTPv3/sender/startTestSendApp.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-fullname="VCMTPv3_SENDER.log"
-filename="${fullname%.*}"
-ext="${fullname##*.}"
-run="_run"
-dot="."
-
 TCPADDR="10.10.1.1"
 TCPPORT="1234"
 MCASTADR="224.0.0.1"
@@ -13,6 +7,3 @@ MCASTPORT="5173"
 INTERFACE="10.10.1.1"
 
 ./testSendApp $TCPADDR $TCPPORT $MCASTADDR $MCASTPORT $INTERFACE day1NGRID.csv
-mv VCMTPv3_SENDER.log $filename$run$i$dot$ext
-
-#mail -s "Experiment done" sschenshawn@gmail.com < /dev/null
