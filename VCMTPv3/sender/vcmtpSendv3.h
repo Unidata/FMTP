d4295350ad05146ca2eb0719d8144fb62b3275fc
Shawn <sschenshawn@gmail.com>
2016-02-08 11:07:08 -0500

    reduced tau_snd to 2 minutes

--- VCMTPv3/sender/vcmtpSendv3.h
+++ VCMTPv3/sender/vcmtpSendv3.h
@@ -95,7 +95,7 @@ public:
                  const unsigned char   ttl = 1,
                  const std::string     ifAddr = "0.0.0.0",
                  const uint32_t        initProdIndex = 0,
-                 const float           tsnd = 15.0);
+                 const float           tsnd = 2.0);
     ~vcmtpSendv3();
 
     void           clearRuninProdSet(int run);
