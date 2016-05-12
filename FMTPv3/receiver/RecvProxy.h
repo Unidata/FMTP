0d5db6e160ddccdb32205c35ccec57a0deb41f6c
Shawn <sschenshawn@gmail.com>
2016-05-12 01:17:47 -0400

    rename RecvAppNotifier to RecvProxy

--- FMTPv3/receiver/RecvProxy.h
+++ FMTPv3/receiver/RecvProxy.h
@@ -3,7 +3,7 @@
  * reserved. See the the file COPYRIGHT in the top-level source-directory for
  * licensing conditions.
  *
- * @file RecvAppNotifier.h
+ * @file RecvProxy.h
  *
  * This file declares the API for classes that notify a receiving application
  * about files.
@@ -11,8 +11,8 @@
  * @author: Steven R. Emmerson
  */
 
-#ifndef FMTP_RECEIVER_RECVAPPNOTIFIER_H_
-#define FMTP_RECEIVER_RECVAPPNOTIFIER_H_
+#ifndef FMTP_RECEIVER_RECVPROXY_H_
+#define FMTP_RECEIVER_RECVPROXY_H_
 
 
 #include <stdint.h>
@@ -22,9 +22,9 @@
 /**
  * This base class notifies a receiving application about events.
  */
-class RecvAppNotifier {
+class RecvProxy {
 public:
-    virtual ~RecvAppNotifier() {};        // definition must exist
+    virtual ~RecvProxy() {};        // definition must exist
 
     /**
      * Notifies the receiving application about the beginning of a product. This
@@ -57,4 +57,4 @@ public:
 };
 
 
-#endif /* FMTP_RECEIVER_RECVAPPNOTIFIER_H_ */
+#endif /* FMTP_RECEIVER_RECVPROXY_H_ */
