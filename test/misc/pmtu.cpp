6e046ef43f603c3080c9dc7960c527a36b91c13e
Shawn <sschenshawn@gmail.com>
2016-05-13 00:24:22 -0400

    added comments for pmtu.cpp

--- test/misc/pmtu.cpp
+++ test/misc/pmtu.cpp
@@ -1,3 +1,30 @@
+/**
+ * Copyright (C) 2015 University of Virginia. All rights reserved.
+ *
+ * @file      pmtu.cpp
+ * @author    Shawn Chen <sc7cq@virginia.edu>
+ * @version   1.0
+ * @date      May 13, 2016
+ *
+ * @section   LICENSE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @brief     Path MTU discovery
+ */
+
+
 #include <stdio.h>
 #include <string.h>
 #include <sys/ioctl.h>
@@ -6,11 +33,12 @@
 
 int main(int argc, char* argv[])
 {
+    /* works for both DGRAM and STREAM */
     int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
     struct ifreq ifr;
     strcpy(ifr.ifr_name, "eth0");
     if(!ioctl(sock, SIOCGIFMTU, &ifr)) {
-        printf("%d\n", ifr.ifr_mtu); // Contains current mtu value
+        printf("%d\n", ifr.ifr_mtu);
     }
     return 0;
 }
